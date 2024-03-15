1888fdc3aa8fbede2d21f6f8655acbc412bd470c
RyuKangHyeon <rkh020727@gmail.com>
2024-03-15 15:31:30 +0900

    may be??

---
+++ 두둥.py
@@ -0,0 +1,17 @@
+print("hello world")
+print(1+1)
+print(2*3) 
+print(2**3)
+print(6/3)
+print(7//3)
+print("A")
+print(type("A")) #str
+print(type(1)) #int
+print(type(2.4)) #float
+print(type(True)) #bool
+#print("A" + 1) Error
+print(1>0.89) #True
+print(3!=3) #Flase
+x=2
+print(x+2) 
+print((x+2)*4)
